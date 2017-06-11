dc848538a830c7fb3d6c1acae02fc7883e5c18ba
Lara Ports <lara.muller@workmail.com>
2017-06-11 19:51:00 -0300

    agora foi

--- OF3_GameParticle/src/Editor/Camera.cpp
+++ OF3_GameParticle/src/Editor/Camera.cpp
@@ -5,35 +5,28 @@ void Camera::init(ofImage& background) {
 
 	maxX = background.getWidth() - ofGetWidth();
 	maxY = background.getHeight() - ofGetHeight();
+	position.set(screen_center);
 }
 
 void Camera::update(float secs) {
-	position.set(screen_center);
 
 	float temp = 0;
 	ofVec2f mouse = ofVec2f(ofGetMouseX(), ofGetMouseY());
 	if (mouse.x < edge) {
-		temp = position.x - (edge - mouse.x) * velocity * secs;
-		temp < 0 ? position.x = temp : 0;
+		position.x += velocity;
 	}
 	//dando mais ou menos certo
 	else if (mouse.x > ofGetWidth() - edge) {
-		temp = position.x - (ofGetWidth() - edge - mouse.x) * velocity * secs;
-		float temp2 = (maxX + position.x);
-		temp2 > 0 ? position.x = temp : 0;
+		position.x -= velocity;
 	}
 
 	if (mouse.y < edge) {
-		temp = position.y + (edge - mouse.y) * velocity * secs;
-		temp < 0 ? position.y = temp : 0;
+		position.y += velocity;
 	}
 	//dando mais ou menos certo
 	else if (mouse.y > ofGetHeight() - edge) {
-		temp = position.y + (ofGetHeight() - edge - mouse.y) * velocity * secs;
-		float temp2 = (maxY + position.y);
-		temp2 > 0 ? position.y = temp : 0;
+		position.y -= velocity;
 	}
-
 }
 
 ofVec2f Camera::getPosition() const {
